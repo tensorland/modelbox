08892acfa7e7c12cd314ec25f8dfa9d04af3999b
diptanu <diptanuc@gmail.com>
2022-08-01 07:32:53 +0000

    Fixed a typo

--- proto/gen_proto.sh
+++ proto/gen_proto.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-protoc --go_out=client --go_opt=paths=source_relative --go-grpc_out=client --go-grpc_opt=paths=source_relative proto/service.proto
+protoc --go_out=client-go --go_opt=paths=source_relative --go-grpc_out=client-go --go-grpc_opt=paths=source_relative proto/service.proto
 
 python -m grpc_tools.protoc -Iproto/ --python_out=client-py/modelbox/ --grpc_python_out=client-py/modelbox/ proto/service.proto

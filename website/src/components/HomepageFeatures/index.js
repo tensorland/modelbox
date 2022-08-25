98de1a3209d22923abe2aefbe4f4198508dfa723
diptanu <diptanuc@gmail.com>
2022-08-25 06:37:44 +0000

    Fix Typos on website

--- website/src/components/HomepageFeatures/index.js
+++ website/src/components/HomepageFeatures/index.js
@@ -4,7 +4,7 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'AI Metatadata Store',
+    title: 'AI Metadata Store',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>

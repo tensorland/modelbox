c3cf379916367235ff9707397b4d1b2c6132edc0
diptanu <diptanuc@gmail.com>
2022-09-24 01:59:12 +0000

    Enabling nbextensions

--- deploy/scripts/jupyter_server_start.sh
+++ deploy/scripts/jupyter_server_start.sh
@@ -6,6 +6,10 @@ pip install jupyter_contrib_nbextensions
 
 jupyter contrib nbextension install --user
 
+pip install jupyter_nbextensions_configurator
+
+jupyter nbextensions_configurator enable --user
+
 
 # Start the nb server
 
